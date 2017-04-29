bff905c127424ec3673301436feda038ab37892d
Shiro Kawai <shiro@acm.org>
2017-04-28 16:55:33 -1000

    Update configure

---
+++ package.scm
@@ -0,0 +1,10 @@
+(define-gauche-package "Gauche-tk"
+  :version "1.0"
+  :description "Simple Tk binding for Gauche"
+  :require (("Gauche" (>= "0.9.5")))
+  :maintainers ("shiro@acm.org")
+  :authors ("Shiro Kawai <shiro@acm.org>")
+  :homepage "http://github.com/shirok/Gauche-tk"
+  :repository "https://github.com/shirok/Gauche-tk"
+  :licenses ("BSD")
+  )
