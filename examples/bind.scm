3e16cd0429e9c9d9d245507aadb305ab74e3184e
Shiro Kawai <shiro@acm.org>
2021-09-19 22:24:15 -1000

    Update string interpolation syntax

--- examples/bind.scm
+++ examples/bind.scm
@@ -3,6 +3,6 @@
 (use tk)
 
 (tk-init '())
-(tk-bind "." '<Key>      (tklambda (%K) (print #`"Pressed ,%K")))
-(tk-bind "." '<Button-1> (tklambda (%x %y) (print #`"Clicked at (,%x ,%y)")))
+(tk-bind "." '<Key>      (tklambda (%K) (print #"Pressed ~%K")))
+(tk-bind "." '<Button-1> (tklambda (%x %y) (print #"Clicked at (~%x ~%y)")))
 (tk-mainloop)
